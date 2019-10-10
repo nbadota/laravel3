e5b418133fc2b78c1dfd124b4506b59b7a03fbcc
nbadota2 <383973155@qq.com>
2019-10-10 10:53:57 +0000

    changing1

--- app/Notifications/ResetPassword.php
+++ app/Notifications/ResetPassword.php
@@ -26,7 +26,7 @@ class ResetPassword extends Notification
         return (new MailMessage)
             ->subject('重置密码')
             ->line('这是一封密码重置邮件，如果是您本人操作，请点击以下按钮继续：')
-            ->action('重置密码', url(route('password.reset', $this->token, false)))
+            ->action('重置密码', url(route('api.password.reset', $this->token, false)))
             ->line('如果您并没有执行此操作，您可以选择忽略此邮件。');
     }
 }
